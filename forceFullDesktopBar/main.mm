0a5593bd2bd53500b835fcd6cf7fb280d2d9aeef
Brian Kendall <guygizmo@gmail.com>
2015-10-12 15:29:35 -0400

    Added command line arguments

--- forceFullDesktopBar/main.mm
+++ forceFullDesktopBar/main.mm
@@ -11,19 +11,24 @@
 #include "injector.h"
 
 @interface Worker : NSObject
-- (void)checkForDock:(NSTimer *)timer;
+- (void)findProcessesAndInjectAfterDelay:(double)delay;
+- (void)doPeriodicCheck:(NSTimer *)timer;
 @end
 
 @implementation Worker {
     NSSet *previousPids;
+    uid_t targetUid;
+    bool hasTargetUid;
 }
 
-- (id)init
+- (id)initWithTargetUid:(uid_t)inTargetUid hasTargetUid:(bool)inHasTargetUid
 {
     self = [super init];
     
     if (self) {
         previousPids = [NSSet set];
+        targetUid = inTargetUid;
+        hasTargetUid = inHasTargetUid;
     }
     
     return self;
@@ -38,32 +43,65 @@
     inj.inject(pid, path);
 }
 
-- (void)checkForDock:(NSTimer *)timer
+// Finds new Dock processes since the last check (or since the process started) and
+// injects our payload into any it finds.
+- (void)findProcessesAndInjectAfterDelay:(double)delay
 {
-    NSSet *currentPids = getAllPIDsForProcessName("Dock");
+    NSSet *currentPids;
+    
+    if (hasTargetUid) {
+        currentPids = getAllPIDsForProcessNameRunByUser("Dock", targetUid);
+    } else {
+        currentPids = getAllPIDsForProcessName("Dock");
+    }
     
     NSMutableSet *newPids = [NSMutableSet setWithSet:currentPids];
     [newPids minusSet:previousPids];
     
     for(NSNumber *pidObj in newPids) {
         NSLog(@"Found Dock process... will inject into pid: %@", pidObj);
-        // Waiting a little bit after discovering the new process, just to make sure it's had ample time to
-        // start up and get running before we mess with it.
-        [self performSelector:@selector(injectIntoProcess:)
-                   withObject:pidObj
-                   afterDelay:1.0];
-    }
         
+        if (delay > 0) {
+            [self performSelector:@selector(injectIntoProcess:)
+                       withObject:pidObj
+                       afterDelay:delay];
+        } else {
+            [self injectIntoProcess:pidObj];
+        }
+    }
+    
     previousPids = currentPids;
+}
+
+- (void)doPeriodicCheck:(NSTimer *)timer
+{
+    // Waiting a little bit after discovering the new process, just to make sure it's had ample time to
+    // start up and get running before we mess with it.
+    [self findProcessesAndInjectAfterDelay:1.0];
 }
 
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Worker *worker = [[Worker alloc] init];
-        [NSTimer scheduledTimerWithTimeInterval:1.0 target:worker selector:@selector(checkForDock:) userInfo:nil repeats:YES];
-        [[NSRunLoop currentRunLoop] run];
+        Worker *worker;
+        NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+        
+        NSUInteger index = [arguments indexOfObject:@"-u"];
+        
+        if (index != NSNotFound && index+1 < [arguments count]) {
+            worker = [[Worker alloc] initWithTargetUid:[arguments[index+1] intValue] hasTargetUid:true];
+        } else {
+            worker = [[Worker alloc] initWithTargetUid:0 hasTargetUid:false];
+        }
+        
+        if ([arguments containsObject:@"-d"]) {
+            [NSTimer scheduledTimerWithTimeInterval:1.0 target:worker selector:@selector(doPeriodicCheck:) userInfo:nil repeats:YES];
+            [[NSRunLoop currentRunLoop] run];
+        } else {
+            NSLog(@"Searching for Dock processes...");
+            [worker findProcessesAndInjectAfterDelay:0];
+        }
     }
     return 0;
 }
