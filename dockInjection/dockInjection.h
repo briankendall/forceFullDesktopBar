edc9135d241ce51974ce98369716d8b8342028cc
Brian Kendall <guygizmo@gmail.com>
2015-10-12 13:34:01 -0400

    Created Xcode project with system utility and dylib targets

---
+++ dockInjection/dockInjection.h
@@ -0,0 +1,13 @@
+//
+//  dockInjection.h
+//  dockInjection
+//
+//  Created by moof on 10/12/15.
+//  Copyright © 2015 Brian Kendall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dockInjection : NSObject
+
+@end
---
+++ dockInjection/dockInjection.m
@@ -0,0 +1,13 @@
+//
+//  dockInjection.m
+//  dockInjection
+//
+//  Created by moof on 10/12/15.
+//  Copyright © 2015 Brian Kendall. All rights reserved.
+//
+
+#import "dockInjection.h"
+
+@implementation dockInjection
+
+@end
