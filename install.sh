fad0b763b7f5fdfe991ed6b432458d2a0decbeba
Brian Kendall <guygizmo@gmail.com>
2015-10-12 16:26:17 -0400

    Added installation / uninstallation scripts and plist for LaunchDaemon

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+mkdir -p /usr/local/forceFullDesktopBar
+cp forceFullDesktopBar /usr/local/forceFullDesktopBar/
+cp bootstrap.dylib /usr/local/forceFullDesktopBar/
+cp dockInjection.dylib /usr/local/forceFullDesktopBar/
+
+echo "Finished"
---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+rm /usr/local/forceFullDesktopBar/forceFullDesktopBar
+rm /usr/local/forceFullDesktopBar/bootstrap.dylib
+rm /usr/local/forceFullDesktopBar/dockInjection.dylib
+rmdir /usr/local/forceFullDesktopBar
+rm /Library/LaunchDaemons/net.briankendall.forceFullDesktopBar.plist
+launchctl remove net.briankendall.forceFullDesktopBar
+
+echo "Uninstalled"
