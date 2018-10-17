63fc47ffa2edab9ef75f5698c4e75a93683be988
Brian Kendall <brian@briankendall.net>
2018-10-17 14:17:35 -0400

    Fixed bad grammar in install script error messages

--- install/install.sh
+++ install/install.sh
@@ -10,17 +10,17 @@ mkdir -p /usr/local/forceFullDesktopBar
 if [ "$?" -ne 0 ]; then echo "Failed to create /usr/local/forceFullDesktopBar"; exit 1; fi
 
 cp forceFullDesktopBar /usr/local/forceFullDesktopBar/
-if [ "$?" -ne 0 ]; then echo "Could not copy forceFullDesktopBar. Make sure you're running this script for the directory that contains: forceFullDesktopBar, bootstrap.dylib, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Could not copy forceFullDesktopBar. Make sure you're running this script in the directory that contains: forceFullDesktopBar, bootstrap.dylib, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
 
 cp bootstrap.dylib /usr/local/forceFullDesktopBar/
-if [ "$?" -ne 0 ]; then echo "Could not copy bootstrap.dylib. Make sure you're running this script for the directory that contains: forceFullDesktopBar, bootstrap.dylib, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Could not copy bootstrap.dylib. Make sure you're running this script in the directory that contains: forceFullDesktopBar, bootstrap.dylib, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
 
 cp dockInjection.dylib /usr/local/forceFullDesktopBar/
-if [ "$?" -ne 0 ]; then echo "Could not copy dockInjection.dylib. Make sure you're running this script for the directory that contains: forceFullDesktopBar, bootstrap.dylib, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Could not copy dockInjection.dylib. Make sure you're running this script in the directory that contains: forceFullDesktopBar, bootstrap.dylib, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
 
 # Copy and launch daemon using launchctl:
 cp net.briankendall.forceFullDesktopBar.plist /Library/LaunchDaemons/
-if [ "$?" -ne 0 ]; then echo "Could not copy LaunchDaemon plist. Make sure you're running this script for the directory that contains: forceFullDesktopBar, bootstrap.dylib, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Could not copy LaunchDaemon plist. Make sure you're running this script in the directory that contains: forceFullDesktopBar, bootstrap.dylib, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
 
 chown root:wheel /Library/LaunchDaemons/net.briankendall.forceFullDesktopBar.plist
 chmod 644 /Library/LaunchDaemons/net.briankendall.forceFullDesktopBar.plist
