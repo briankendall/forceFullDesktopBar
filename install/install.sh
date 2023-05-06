80d1a396c12c1a962b14db034b29939c010ada08
Brian Kendall <brian@briankendall.net>
2023-05-06 15:58:17 -0400

    Improve install / uninstall scripts The install script now properly removes any existing copies of forceFullDesktopBar before proceeding with the installation, and both scripts now use a better method for restarting the Dock.

--- install/install.sh
+++ install/install.sh
@@ -5,23 +5,63 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Remove bootstrap.dylib if it exists, since it's no longer needed
-if test -f "/usr/local/forceFullDesktopBar/bootstrap.dylib"; then
-    rm /usr/local/forceFullDesktopBar/bootstrap.dylib
+# Deactivate old launchctl if it's running
+
+prevPID=$(pgrep forceFullDesktopBar)
+
+if [ ! -z "$prevPID" ]; then
+    launchctlInfo=`launchctl list | grep net.briankendall.forceFullDesktopBar`
+    
+    if [ -z "$launchctlInfo" ]; then
+        killall -QUIT forceFullDesktopBar
+    else
+        echo Removing old launch daemon...
+        launchctl remove net.briankendall.forceFullDesktopBar
+    fi
+    
+    echo "Waiting for previous forceFullDesktopBar to terminate..."
+    while s=`ps -p $prevPID -o stat=` && [[ "$s" && "$s" != 'Z' ]]; do
+        sleep 0.1
+    done
+    
+    echo "Restarting Dock..."
+    interactiveUID=`scutil <<< "show State:/Users/ConsoleUser" | awk '/UID :/ { print $3 }'`
+    # The safe way to restart the Dock:
+    launchctl asuser $interactiveUID launchctl stop com.apple.Dock.agent
+    launchctl asuser $interactiveUID launchctl start com.apple.Dock.agent
 fi
 
-# Copy files:
 mkdir -p /usr/local/forceFullDesktopBar
 if [ "$?" -ne 0 ]; then echo "Failed to create /usr/local/forceFullDesktopBar"; exit 1; fi
 
-cp forceFullDesktopBar /usr/local/forceFullDesktopBar/
+# Remove previous files if they exist
+# Apparently it's important to delete the existing files first, otherwise the
+# new dylibs may not be properly injected after loading the launch daemon.
+
+if [ -f "/usr/local/forceFullDesktopBar/bootstrap.dylib" ]; then
+    rm /usr/local/forceFullDesktopBar/bootstrap.dylib
+fi
+
+if [ -f "/usr/local/forceFullDesktopBar/dockInjection.dylib" ]; then
+    rm /usr/local/forceFullDesktopBar/dockInjection.dylib
+fi
+
+if [ -f "/usr/local/forceFullDesktopBar/forceFullDesktopBar" ]; then
+    rm /usr/local/forceFullDesktopBar/forceFullDesktopBar
+fi
+
+if [ -f "/Library/LaunchDaemons/net.briankendall.forceFullDesktopBar.plist" ]; then
+    rm /Library/LaunchDaemons/net.briankendall.forceFullDesktopBar.plist
+fi
+
+cp forceFullDesktopBar /usr/local/forceFullDesktopBar/forceFullDesktopBar
 if [ "$?" -ne 0 ]; then echo "Could not copy forceFullDesktopBar. Make sure you're running this script in the directory that contains: forceFullDesktopBar, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
 
-cp dockInjection.dylib /usr/local/forceFullDesktopBar/
+cp dockInjection.dylib /usr/local/forceFullDesktopBar/dockInjection.dylib
 if [ "$?" -ne 0 ]; then echo "Could not copy dockInjection.dylib. Make sure you're running this script in the directory that contains: forceFullDesktopBar, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
 
 # Copy and launch daemon using launchctl:
-cp net.briankendall.forceFullDesktopBar.plist /Library/LaunchDaemons/
+cp -f net.briankendall.forceFullDesktopBar.plist /Library/LaunchDaemons/net.briankendall.forceFullDesktopBar.plist
 if [ "$?" -ne 0 ]; then echo "Could not copy LaunchDaemon plist. Make sure you're running this script in the directory that contains: forceFullDesktopBar, dockInjection.dylib, net.briankendall.forceFullDesktopBar.plist "; exit 1; fi
 
 chown root:wheel /Library/LaunchDaemons/net.briankendall.forceFullDesktopBar.plist
--- install/uninstall.sh
+++ install/uninstall.sh
@@ -5,6 +5,8 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+interactiveUID=`scutil <<< "show State:/Users/ConsoleUser" | awk '/UID :/ { print $3 }'`
+
 if test -f "/usr/local/forceFullDesktopBar/bootstrap.dylib"; then
     rm /usr/local/forceFullDesktopBar/bootstrap.dylib
 fi
@@ -15,8 +17,19 @@ rmdir /usr/local/forceFullDesktopBar
 rm /Library/LaunchDaemons/net.briankendall.forceFullDesktopBar.plist
 launchctl remove net.briankendall.forceFullDesktopBar
 
-# Give app a moment to fully quit:
-sleep 0.5
-killall Dock
+# Wait for app to fully quit:
+
+prevPID=$(pgrep forceFullDesktopBar)
+if [ ! -z "$prevPID" ]; then
+    echo "Waiting for forceFullDesktopBar to terminate..."
+    while s=`ps -p $prevPID -o stat=` && [[ "$s" && "$s" != 'Z' ]]; do
+        sleep 0.1
+    done
+fi
+
+echo "Restarting Dock..."
+# The safe way to restart the Dock:
+launchctl asuser $interactiveUID launchctl stop com.apple.Dock.agent
+launchctl asuser $interactiveUID launchctl start com.apple.Dock.agent
 
 echo "Uninstalled"
